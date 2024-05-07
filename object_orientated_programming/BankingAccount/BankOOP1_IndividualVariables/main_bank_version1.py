682bc73009df986960056b20e571ad84f6f28c39
jebb king <gabouchard2002@gmail.com>
2024-05-07 18:45:31 +0300

    extend test code & include new account user

--- object_orientated_programming/BankingAccount/BankOOP1_IndividualVariables/main_bank_version1.py
+++ object_orientated_programming/BankingAccount/BankOOP1_IndividualVariables/main_bank_version1.py
@@ -21,3 +21,22 @@ o_marys_account.deposit(100, 'MarysPassword')
 # Отображаем счета
 o_joes_account.show()
 o_marys_account.show()
+
+# Создаем новый счет с информацией от пользователя
+print()
+user_name = input('What is the name for the new user account? ')
+user_balance = input('What is the starting balance for this account? ')
+user_password = input('What is the password you want to use for this '
+                      'account? ')
+o_new_account = Account(user_name, user_balance, user_password)
+
+# отображаем вновь созданный счет пользователя
+o_new_account.show()
+
+o_new_account.deposit(100, user_password)
+user_balance = o_new_account.get_balance(user_password)
+print()
+print('After depositing 100, the user`s balance is:', user_balance)
+
+# Отображаем новый счет
+o_new_account.show()
